819357a7f9be9e44b74c52a32b40cf0d4929c482
turjo4620 <sarkerturjo2022@gmail.com>
2026-04-04 16:55:40 +0600

    added

---
+++ BipartiteGraph.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Graph{
+    int V;
+    vector<vector<int>>adj;
+
+public:
+    Graph(int V){
+        this->V = V;
+        adj.resize(V);
+    }
+
+    void addEdge(int u, int v){
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+
+    bool bfs_bipartite_checker(int root, vector<int>&color){
+        queue<int>q;
+
+        q.push(root);
+        color[root] = 0;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            for(int neighbour : adj[node]){
+                if(color[neighbour] == -1){
+                    color[neighbour] = 1 - color[node];
+                    q.push(neighbour);
+                }
+                else if(color[neighbour] == color[node]){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
+
+    }
+
+};
+
+
+int main(){
+    int V, E;
+    cin>>V>>E;
+    Graph g(V);
+
+    for(int i = 0; i < E; i++){
+        int a , b;
+        cin>>a>>b;
+        g.addEdge(a, b);
+    }
+
+    vector<int>color(V, -1);
+
+    for(int i = 0; i < V; i++){
+        if(color[i] == -1){
+            if(!g.bfs_bipartite_checker(i, color)){
+                cout<<false;
+                return 0;
+            }
+        }
+    }
+    cout<<true;
+    
+    return 0;
+}
